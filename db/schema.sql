e1ab2c260fd11fd72827f5698df8d441ad139b5f
Adam Tilly <tillyjadam@gmail.com>
2021-11-05 23:13:05 -0500

    added tables, database, and seed sql code

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS job_title;
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE job_title (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  title_id INTEGER,
+  FOREIGN KEY (title_id) REFERENCES job_title(id) ON DELETE SET NULL,
+  manager_id INTEGER ,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO department (dept_name)
+VALUES
+('Design Engineering'),
+('Sales'),
+('Purchasing'),
+('Executive');
+
+INSERT INTO job-title (title,salary,department_id)
+VALUES
+  ('Engineer I', 55,000,1),
+  ('Engineer II', 65,000,1),
+  ('Engineer III', 75,000,1),
+  ('Team Manager', 85,000,2),
+  ('Department Manager', 95,000,1),
+  ('Sales Coordinator', 55,000,2),
+  ('Senior Sales Cordinator', 75,000,2),
+  ('jr Purchaser', 60,000,3),
+  ('Sr Purchaser', 70,000,3),
+  ('Managing Purchaser', 90,000,3),
+  ('CEO', 260,000,4),
+  ('Vice President', 160,000,4);
+ 
+  INSERT INTO voters (first_name, last_name, title_id, manager_id)
+VALUES
+  ('Joe', 'Lidberg', 5,2),
+  ('Mark', 'Anderson', 11,3),
+  ('David', 'Kellin', 10,NULL),
+  ('Nick', 'Grundlow',3,1),
+  ('Tyler', 'Pojonowski',3,1),
+  ('Jake', 'Eckroad',2,1),
+  ('Seth', 'Sanderson',1,1),
+  ('Kim', 'Howlsklaw',8,9),
+  ('Jeff', 'boyde',9,3),
+  ('Mike', 'Wilert',8,9),
+  ('Kelsey', 'Tillman',7,9),
+  ('Scott', 'johnson',5,14),
+  ('Chris', 'Smith',6,14),
+  ('Dave', 'Gunderson',6,3);
+
+  
